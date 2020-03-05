be29d795d28d249a7e7e16f15c15644c2c63196c
youp bos <2107418@talnet.nl>
2020-03-05 12:03:34 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,7 +1,16 @@
-INSERT INTO planeten(diameter, afstand_tot_de_zon, massa)
-VALUES
-(1392000, 0, 332.946),
-(4880, 57910000, 0.1),
-(12104,108208930 ,0.9),
-(12.56,149597870 ,1),
-(6794,227936640 ,0.1);
+-- ALTER TABLE planeten
+-- MODIFY diameter BIGINT NOT NULL;
+-- ALTER TABLE planeten
+-- MODIFY afstand_tot_de_zon BIGINT NOT NULL;
+-- ALTER TABLE planeten
+-- MODIFY massa BIGINT NOT NULL;
+
+-- ALTER TABLE planeten ADD
+-- (
+-- bezoek_datum NOW() NOT NULL
+-- );
+
+-- UPDATE planeten 
+-- SET bezoek_datum = NOW();
+
+-- SET @@time_zone = "sytem";
